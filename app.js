b72ed0091bd8d369de0a2cd96ef9b2bdbce2a09a
LucasParra95 <lucasandresparra@gmail.com>
2025-02-12 00:18:03 -0300

    Diseño responsive de home, about y skills( sin el timeline )

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const links = document.querySelectorAll('.link');
 const sections = document.querySelectorAll('section');
 
+const mobileMenu = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.links-group');
+
+mobileMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
 let activeLink = 0;
 
 links.forEach((link, i) =>{
